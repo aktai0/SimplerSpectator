7f45c61567983e3a260d859081cc902e8c59563f
aktai0 <xtoken.x@gmail.com>
2017-07-11 16:47:30 -0700

    Reload API if the API key file was changed

--- SpectatorFunctions/APIHelper.vb
+++ SpectatorFunctions/APIHelper.vb
@@ -1,9 +1,11 @@
 ﻿Imports RiotSharp
+Imports System.IO
 
 Public Class APIHelper
    Private Const API_KEY_FILE As String = "riot_key"
    Private Shared API_KEY As String = ""
 
+   Private Shared LastWriteTime As DateTime
    Private Shared Sub API_LOAD_FILE()
       Try
          Using reader As New IO.StreamReader(APIHelper.API_KEY_FILE)
@@ -11,10 +13,17 @@ Public Class APIHelper
          End Using
 
          RiotApi.GetInstance(APIHelper.API_KEY)
+         LastWriteTime = GetAPIKeyLastWriteTime()
       Catch ex As Exception
          MsgBox("Error reading riot_key file. Please check README for instructions.")
       End Try
    End Sub
+
+   Private Shared ReadOnly Property GetAPIKeyLastWriteTime() As DateTime
+      Get
+         Return (New FileInfo(APIHelper.API_KEY_FILE)).LastWriteTime
+      End Get
+   End Property
 
    Public Shared Sub API_INIT()
       If API_KEY = "" Then
@@ -27,6 +36,10 @@ Public Class APIHelper
          If API_KEY = "" Then
             API_INIT()
          End If
+         If LastWriteTime < GetAPIKeyLastWriteTime Then
+            API_LOAD_FILE()
+         End If
+         ' Check if API key file modified was newer than the previous value
          Return RiotApi.GetInstance(API_KEY)
       End Get
    End Property
